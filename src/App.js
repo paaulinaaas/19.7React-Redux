48d7eb6f42db20bd79f5645d55054212653a3f37
Paulina Sikora <paaulinaaas@gmail.com>
2017-10-24 19:56:21 +0200

    done

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      Tu będzie lista komentarzy!
+    </div>
+  );
+};
+
+export default App;
+
